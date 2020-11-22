1b1c198496559d95ff9d749d50efc28d09adbfe7
meganallred <meg21allred@gmail.com>
2020-11-21 21:39:14 -0500

    trying this

--- server.js
+++ server.js
@@ -27,25 +27,26 @@ app.listen(process.env.PORT);
 
 app.get("/", async (req, res) => {
 
-    //var articles;
+    var articles;
 
     try {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM blog_entries');
         const results = { 'results': (result) ? result.rows : null};
         //res.send(JSON.stringify(results));
-        res.json(results);
+        //res.json(results);
+        articles = results;
         client.release();
     } catch (err) {
         console.error(err);
         res.send("Error " + err);
     }
     
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    }];
+    // const articles = [{
+    //     title: 'Test Article',
+    //     createdAt: new Date(),
+    //     description: 'Test description'
+    // }];
 
    res.render('articles/index', { articles: articles});
 });
