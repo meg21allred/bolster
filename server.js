7b826e0150b3ca00d3affb77fc3f6da092922e73
meganallred <meg21allred@gmail.com>
2020-11-22 00:03:50 -0500

    grasping

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const parser = require("body-parser");
 //const { Pool } = require("pg");
 
 const app = express();
-//process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const { Client } = require('pg');
 
