25f905ebe060e51ed663e935c8ffe35f7a63ec45
meganallred <meg21allred@gmail.com>
2020-11-28 18:34:33 -0500

    commenting out stuff

--- routes/articles.js
+++ routes/articles.js
@@ -8,69 +8,69 @@ router.use(express.urlencoded({extended: true}));
 
 
 const { Pool } = require('pg');
-let connectionString = process.env.DATABASE_URL/* || "postgres://bolster21:bolster1521@localhost:5432/bolsterdb"*/;
+let connectionString = process.env.DATABASE_URL; /* || "postgres://bolster21:bolster1521@localhost:5432/bolsterdb"*/
 const pool = new Pool({
     connectionString: connectionString, ssl: true
 });
 
 
-router.get('/login', (req, res) => {
-    res.render('articles/login.ejs');
-})
+// router.get('/login', (req, res) => {
+//     res.render('articles/login.ejs');
+// })
 
-router.post('/login', async (req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
+// router.post('/login', async (req, res) => {
+//     var username = req.body.username;
+//     var password = req.body.password;
     
-    var params =  [username];
-    try {
-        const client = await pool.connect();
-        const result = await client.query("SELECT user_password FROM user_account WHERE username = $1", params);
-        var hash = result.rows[0].user_password;
-        bcrypt.compare(password, hash, function (err, hashResult) {
-            if (hashResult) {
-                console.log('access granted');
-                //store session variable into user here for local host
-                //req.session.username = username;
-                res.redirect('/');
-            } else {
-                console.log('access denied');  
-            }
-        })
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
+//     var params =  [username];
+//     try {
+//         const client = await pool.connect();
+//         const result = await client.query("SELECT user_password FROM user_account WHERE username = $1", params);
+//         var hash = result.rows[0].user_password;
+//         bcrypt.compare(password, hash, function (err, hashResult) {
+//             if (hashResult) {
+//                 console.log('access granted');
+//                 //store session variable into user here for local host
+//                 //req.session.username = username;
+//                 res.redirect('/');
+//             } else {
+//                 console.log('access denied');  
+//             }
+//         })
+//         client.release();
+//     } catch (err) {
+//         console.error(err);
+//         res.send("Error " + err);
+//     }
     
-});
+// });
 
-router.get('/register', (req, res) => {
-    res.render('articles/register.ejs');
-})
+// router.get('/register', (req, res) => {
+//     res.render('articles/register.ejs');
+// })
 
-router.post('/register', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+// router.post('/register', async (req, res) => {
+//     try {
+//         const hashedPassword = await bcrypt.hash(req.body.password, 10)
      
-        var username = req.body.username;
-        var email = req.body.email;
-        var password = hashedPassword;
+//         var username = req.body.username;
+//         var email = req.body.email;
+//         var password = hashedPassword;
 
-        var params = [username, email, password];
-        try {
-            const client = await pool.connect();
-            const result = await client.query("INSERT INTO user_account (username, user_email, user_password) VALUES ($1, $2, $3)", params);
-            client.release();
-        } catch (err) {
-            console.error(err);
-            res.send("Error " + err);
-        }
-        res.redirect('/articles/login')
-    } catch {
-        res.redirect('/articles/register')
-    }
-})
+//         var params = [username, email, password];
+//         try {
+//             const client = await pool.connect();
+//             const result = await client.query("INSERT INTO user_account (username, user_email, user_password) VALUES ($1, $2, $3)", params);
+//             client.release();
+//         } catch (err) {
+//             console.error(err);
+//             res.send("Error " + err);
+//         }
+//         res.redirect('/articles/login')
+//     } catch {
+//         res.redirect('/articles/register')
+//     }
+// })
 
 
 router.get('/new', (req, res) => {
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const { Pool } = require('pg');
-let connectionString = process.env.DATABASE_URL /*|| "postgres://bolster21:bolster1521@localhost:5432/bolsterdb"*/;
+let connectionString = process.env.DATABASE_URL; /*|| "postgres://bolster21:bolster1521@localhost:5432/bolsterdb"*/
 const pool = new Pool({
     connectionString: connectionString, ssl: true
 });
@@ -45,38 +45,38 @@ app.get("/", async (req, res) => {
     
 });
 
-app.get('/random', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM blog_entries ');
+// app.get('/random', async (req, res) => {
+//     try {
+//         const client = await pool.connect();
+//         const result = await client.query('SELECT * FROM blog_entries ');
         
-        var minimum = 0;
-        var maximum = result.rows.length - 1;
-        var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-        var randomBlogPost = `
-        <div class="container" id="randomDiv">
-          <h1 >Random blog post</h1>
-                <div class="card mt-4">
-                    <div class="card-body">
-                        <h4 class="card-title">${result.rows[randomnumber].title}</h4>
-                        <div class="card-subtitle text-muted mb-2">
-                        ${result.rows[randomnumber].blog_date.toLocaleDateString()}
-                        </div>
-                        <div class="card-text mb-2">${result.rows[randomnumber].blog_description}</div>
-                    </div>
-                </div>
-                <br>
-                <button class="btn btn-success" id="randomButton" onclick="getRandomBlog()">Get New Entry</button>
+//         var minimum = 0;
+//         var maximum = result.rows.length - 1;
+//         var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+//         var randomBlogPost = `
+//         <div class="container" id="randomDiv">
+//           <h1 >Random blog post</h1>
+//                 <div class="card mt-4">
+//                     <div class="card-body">
+//                         <h4 class="card-title">${result.rows[randomnumber].title}</h4>
+//                         <div class="card-subtitle text-muted mb-2">
+//                         ${result.rows[randomnumber].blog_date.toLocaleDateString()}
+//                         </div>
+//                         <div class="card-text mb-2">${result.rows[randomnumber].blog_description}</div>
+//                     </div>
+//                 </div>
+//                 <br>
+//                 <button class="btn btn-success" id="randomButton" onclick="getRandomBlog()">Get New Entry</button>
 
-        `
-        res.send(randomBlogPost);
-        client.release();
+//         `
+//         res.send(randomBlogPost);
+//         client.release();
         
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-})
+//     } catch (err) {
+//         console.error(err);
+//         res.send("Error " + err);
+//     }
+// })
 
 app.use('/articles', articleRouter);
 
