a7647bf18429cbc009558d2746c1596aa9770d67
meganallred <meg21allred@gmail.com>
2020-12-03 10:31:26 -0500

    adding sessions

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const articleRouter = require('./routes/articles');
-//const session = require('express-session');
+const session = require('express-session');
 //const bodyParser = require('body-parser');
 const app = express();
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
