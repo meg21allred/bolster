fb4eeac2cba426b716682a3a4c504985273c5fce
meganallred <meg21allred@gmail.com>
2020-12-03 11:27:45 -0500

    adding bcrypt to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const articleRouter = require('./routes/articles');
 const session = require('express-session');
 //const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const app = express();
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
